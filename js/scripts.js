7441d309bbd1cc9c5673de2003637f32c575ce7b
Ashton Walden <ashtonwalden@icloud.com>
2026-02-22 16:01:30 -0700

    started JS, getting vals

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("I am connected");
+document.querySelector('#calculate').addEventListener('click', () => {
+    // console.log('clicked')
+    const width = Number(document.querySelector('#width').value);
+    const height = Number(document.querySelector('#width').value);
+    const depth = Number(document.querySelector('#width').value);
+    const paintType = document.querySelector('#quality').selectedOptions[0].text;
+    const quality = document.querySelector('#quality').selectedOptions[0].value;
+    // console.log(paintType)
+    // console.log(quality)
+}) // end of calculate function
